c98fdbe235b55a94cb27d9b539ee1a3c403bb31b
tm990032 <Saito@saito-mac.local>
2016-09-02 16:48:26 +0900

    交通マップを表示

--- openstreetmap/1.js
+++ openstreetmap/1.js
@@ -1,7 +1,7 @@
 var initMap = function() {
     var map = L.map('map');
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    L.tileLayer('http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
